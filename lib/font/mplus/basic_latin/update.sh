0af96aba456ee460e9bb5defe393891e3e2bbdc4
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2016-08-05 19:27:12 +0900

    Add a script to download M+ webfonts

---
+++ lib/font/mplus/basic_latin/update.sh
@@ -0,0 +1,155 @@
+#!/bin/sh
+
+curl \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2p-thin-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2p-thin-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2p-thin-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2p-regular-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2p-regular-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2p-regular-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2p-medium-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2p-medium-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2p-medium-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2p-light-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2p-light-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2p-light-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2p-heavy-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2p-heavy-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2p-heavy-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2p-bold-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2p-bold-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2p-bold-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2p-black-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2p-black-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2p-black-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2m-thin-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2m-thin-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2m-thin-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2m-regular-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2m-regular-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2m-regular-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2m-medium-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2m-medium-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2m-medium-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2m-light-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2m-light-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2m-light-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2m-bold-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2m-bold-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2m-bold-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2c-thin-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2c-thin-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2c-thin-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2c-regular-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2c-regular-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2c-regular-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2c-medium-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2c-medium-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2c-medium-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2c-light-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2c-light-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2c-light-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2c-heavy-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2c-heavy-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2c-heavy-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2c-bold-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2c-bold-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2c-bold-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2c-black-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2c-black-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-2c-black-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1p-thin-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1p-thin-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1p-thin-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1p-regular-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1p-regular-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1p-regular-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1p-medium-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1p-medium-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1p-medium-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1p-light-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1p-light-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1p-light-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1p-heavy-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1p-heavy-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1p-heavy-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1p-bold-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1p-bold-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1p-bold-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1p-black-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1p-black-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1p-black-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1mn-thin-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1mn-thin-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1mn-thin-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1mn-regular-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1mn-regular-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1mn-regular-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1mn-medium-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1mn-medium-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1mn-medium-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1mn-light-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1mn-light-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1mn-light-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1mn-bold-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1mn-bold-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1mn-bold-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1m-thin-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1m-thin-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1m-thin-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1m-regular-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1m-regular-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1m-regular-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1m-medium-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1m-medium-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1m-medium-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1m-light-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1m-light-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1m-light-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1m-bold-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1m-bold-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1m-bold-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1c-thin-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1c-thin-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1c-thin-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1c-regular-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1c-regular-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1c-regular-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1c-medium-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1c-medium-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1c-medium-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1c-light-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1c-light-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1c-light-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1c-heavy-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1c-heavy-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1c-heavy-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1c-bold-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1c-bold-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1c-bold-sub.eot \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1c-black-sub.woff \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1c-black-sub.ttf \
+    -O https://mplus-fonts.osdn.jp/webfonts/basic_latin/mplus-1c-black-sub.eot
+
+for f in *-regular-*.ttf; do
+  f=${f#mplus-}
+  type=${f%%-*}
+
+  css=mplus-$type.css
+  echo "/* M+ $type */" > $css
+  for g in *-$type-*.ttf; do
+      g=${g#mplus-$type-}
+      weight=${g%%-*}
+
+      echo ""
+      echo "@font-face"
+      echo "{"
+      echo "  font-family: 'M+ $type';"
+      echo "  src: url('mplus-$type-$weight-sub.eot');"
+      echo "  src: url('mplus-$type-$weight-sub.eot?#iefix') format('embedded-opentype'),"
+      echo "       url('mplus-$type-$weight-sub.woff') format('woff'),"
+      echo "       url('mplus-$type-$weight-sub.ttf') format('truetype');"
+      echo "  font-weight: $weight;"
+      echo "}"
+  done >> $css
+done
